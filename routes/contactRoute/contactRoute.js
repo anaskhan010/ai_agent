40d764dba07bdfd277d929fbcc15bd3a4c0729ba
unknown <anas@sentrixmedia.com>
2024-11-21 16:01:17 +0500

    multiple lists contacts added

--- routes/contactRoute/contactRoute.js
+++ routes/contactRoute/contactRoute.js
@@ -6,7 +6,7 @@ const authMiddleware = require("../../middleware/authMiddleware");
 router.get("/contacts/:page", contactController.getAllContacts);
 
 router.get("/contacts/lists", contactController.getListNamesWithContactCount);
-router.get("/contacts/:id", authMiddleware, contactController.getContactById);
+router.get("/contact/:id", authMiddleware, contactController.getContactById);
 router.post("/contacts", authMiddleware, contactController.createContacts); // For multiple contacts
 router.put("/contacts/:id", authMiddleware, contactController.updateContact);
 router.delete("/contacts/:id", authMiddleware, contactController.deleteContact);
